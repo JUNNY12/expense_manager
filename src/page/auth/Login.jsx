14775c9f88540f9d8bbd14023f7ec8ade4751199
unknown <juwonemmanuel22@gmail.com>
2023-03-24 22:31:20 +0100

    styles: Adjusted login and register form positioning

--- src/page/auth/Login.jsx
+++ src/page/auth/Login.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 
 const Login = () => {
 
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
 
     const [values, handleValueChange] = useForm({
@@ -36,86 +36,88 @@ const Login = () => {
         e.preventDefault()
         console.log("email:", email, "password:", password)
         console.log('login')
-        navigate ('/')
+        navigate('/')
     }
 
     return (
         <div className='loginWrapper'>
-            <div className='userIcon'>
-                <UserLoginIcon />
-            </div>
-            <h1 className='text-center formHeader'>Login</h1>
-            <form onSubmit={handleSubmit}>
+            <div className='loginFormWrap'>
+                <div className='userIcon'>
+                    <UserLoginIcon />
+                </div>
+                <h1 className='text-center formHeader'>Login</h1>
+                <form onSubmit={handleSubmit}>
 
-                <div className='mb-3 inputWrapper'>
-                    <div className='iconWrap'>
-                        <EmailIcon />
+                    <div className='mb-3 inputWrapper'>
+                        <div className='iconWrap'>
+                            <EmailIcon />
+                        </div>
+                        <input
+                            ref={inputRef}
+                            required
+                            aria-required='true'
+                            className='loginInput'
+                            name='email'
+                            value={email}
+                            onChange={handleValueChange}
+                            type={`email`}
+                            aria-label='email'
+                            placeholder='Enter your email'
+                        />
                     </div>
-                    <input
-                        ref={inputRef}
-                        required
-                        aria-required='true'
-                        className='loginInput'
-                        name='email'
-                        value={email}
-                        onChange={handleValueChange}
-                        type={`email`}
-                        aria-label='email'
-                        placeholder='Enter your email'
-                    />
-                </div>
 
-                <div className='mb-3 inputWrapper'>
-                    <div className='iconWrap'>
-                        <PasswordIcon />
+                    <div className='mb-3 inputWrapper'>
+                        <div className='iconWrap'>
+                            <PasswordIcon />
+                        </div>
+                        <input
+                            required
+                            aria-required='true'
+                            className='loginInput'
+                            name='password'
+                            value={password}
+                            onChange={handleValueChange}
+                            type={inputType}
+                            aria-label='password'
+                            placeholder='Enter your password'
+                        />
+                        <button
+                            disabled={password.length === 0}
+                            className='eye'
+                            type='button'
+                            onClick={togglePasswordVisiblity}
+                        >
+                            {showPassword ?
+                                <EyeSlashIcon />
+                                :
+                                <EyeIcon />
+                            }
+                        </button>
+
                     </div>
-                    <input
-                        required
-                        aria-required='true'
-                        className='loginInput'
-                        name='password'
-                        value={password}
-                        onChange={handleValueChange}
-                        type={inputType}
-                        aria-label='password'
-                        placeholder='Enter your password'
-                    />
-                    <button
-                        disabled={password.length === 0}
-                        className='eye'
-                        type='button'
-                        onClick={togglePasswordVisiblity}
-                    >
-                        {showPassword ?
-                            <EyeSlashIcon />
-                            :
-                            <EyeIcon />
-                        }
-                    </button>
 
+                    <div className='mb-2'>
+                        <input id='check' type="checkbox" />
+                        <label htmlFor="check" className='ms-2 check'>Remember Me</label>
+                    </div>
+
+                    <button className='loginBtn btn-2'>Login</button>
+                </form>
+                <div className='mt-3 fs-6 fw-bold'>
+                    <p className='ques text-center'>
+                        <span className='me-2 dont'> Dont Have an account yet?</span> <br />
+                        <Link to={`/register`}>
+                            <span className='signUp'> Create an Account</span>
+                        </Link>
+                    </p>
                 </div>
-
-                <div className='mb-2'>
-                    <input id='check' type="checkbox" />
-                    <label htmlFor="check" className='ms-2 check'>Remember Me</label>
-                </div>
-
-                <button className='loginBtn btn-2'>Login</button>
-            </form>
-            <div className='mt-3 fs-6 fw-bold'>
-                <p className='ques text-center'>
-                    <span className='me-2 dont'> Dont Have an account yet?</span> <br />
-                    <Link to={`/register`}>
-                        <span className='signUp'> Create an Account</span>
-                    </Link>
-                </p>
-            </div>
-            <div className='google' typeof='button' aria-label='button'>
-                <div>
-                    <span className='me-4'>
-                        <GoogleIcon />
-                    </span>
-                    <span>Sign in with Google</span>
+                <div className='google' typeof='button' aria-label='button'>
+                    <div>
+                        <span className='me-4'>
+                            <GoogleIcon />
+                        </span>
+                        <span>Sign in with Google</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/page/auth/Register.jsx
+++ src/page/auth/Register.jsx
@@ -31,73 +31,75 @@ const Register = () => {
 
   return (
     <div className='registerWrapper'>
-      <div className='userIcon'>
-        <UserLoginIcon />
-      </div>
-      <h1 className='text-center formHeader'>Create an Account</h1>
-      <form onSubmit={handleSubmit}>
-
-        <div className='mb-3 inputWrapper'>
-          <div className='iconWrap'>
-            <User />
-          </div>
-          <input
-            ref={inputRef}
-            className='registerInput'
-            name='username'
-            value={username}
-            onChange={handleValueChange}
-            type={`text`}
-            aria-label='Name'
-            placeholder='Enter your username'
-          />
+      <div className="registerFormWrap">
+        <div className='userIcon'>
+          <UserLoginIcon />
         </div>
-        <div className='mb-3 inputWrapper'>
-          <div className='iconWrap'>
-            <EmailIcon />
-          </div>
-          <input
-            className='registerInput'
-            name='email'
-            value={email}
-            onChange={handleValueChange}
-            type={`email`}
-            aria-label='email'
-            placeholder='Enter your email'
-          />
-        </div>
+        <h1 className='text-center formHeader'>Create an Account</h1>
+        <form onSubmit={handleSubmit}>
 
-        <div className='mb-3 inputWrapper'>
-          <div className='iconWrap'>
-            <PasswordIcon />
+          <div className='mb-3 inputWrapper'>
+            <div className='iconWrap'>
+              <User />
+            </div>
+            <input
+              ref={inputRef}
+              className='registerInput'
+              name='username'
+              value={username}
+              onChange={handleValueChange}
+              type={`text`}
+              aria-label='Name'
+              placeholder='Enter your username'
+            />
           </div>
-          <input
-            className='registerInput'
-            name='password'
-            value={password}
-            onChange={handleValueChange}
-            type={`password`}
-            aria-label='password'
-            placeholder='Enter your password'
-          />
-        </div>
+          <div className='mb-3 inputWrapper'>
+            <div className='iconWrap'>
+              <EmailIcon />
+            </div>
+            <input
+              className='registerInput'
+              name='email'
+              value={email}
+              onChange={handleValueChange}
+              type={`email`}
+              aria-label='email'
+              placeholder='Enter your email'
+            />
+          </div>
 
-        <button className='registerBtn btn-2'>Register</button>
-      </form>
-      <div className='mt-3 fs-6 fw-bold'>
-        <p className='ques'>
-          <span className='me-2 dont'> Already have an account?</span>
-          <Link to={`/login`}>
-            <span className='signUp'> Login</span>
-          </Link>
-        </p>
-      </div>
-      <div className='google' typeof='button' aria-label='button'>
-        <div>
-          <span className='me-4'>
-            <GoogleIcon />
-          </span>
-          <span>Sign up with Google</span>
+          <div className='mb-3 inputWrapper'>
+            <div className='iconWrap'>
+              <PasswordIcon />
+            </div>
+            <input
+              className='registerInput'
+              name='password'
+              value={password}
+              onChange={handleValueChange}
+              type={`password`}
+              aria-label='password'
+              placeholder='Enter your password'
+            />
+          </div>
+
+          <button className='registerBtn btn-2'>Register</button>
+        </form>
+        <div className='mt-3 fs-6 fw-bold'>
+          <p className='ques'>
+            <span className='me-2 dont'> Already have an account?</span>
+            <Link to={`/login`}>
+              <span className='signUp'> Login</span>
+            </Link>
+          </p>
+        </div>
+        <div className='google' typeof='button' aria-label='button'>
+          <div>
+            <span className='me-4'>
+              <GoogleIcon />
+            </span>
+            <span>Sign up with Google</span>
+          </div>
         </div>
       </div>
     </div>
